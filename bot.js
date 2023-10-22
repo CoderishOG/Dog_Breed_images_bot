ebbd755bc9e5334bb1a7e85dabe59c446eb50faa
Rishabh <rishabh09p@gmail.com>
2023-10-22 14:58:35 +0530

    ,,

--- bot.js
+++ bot.js
@@ -5,12 +5,12 @@ bot.catch((err, ctx) => {
   console.error(`Error for ${ctx.updateType}`, err);
 });
 
-bot.start((ctx) => {
-  ctx.reply(`Hello there ${ctx.message.from.first_name}!! and Welcome to Dog Breed Images Bot ❤️.
+bot.start(async (ctx) => {
+  await ctx.reply(`Hello there ${ctx.message.from.first_name}!! and Welcome to Dog Breed Images Bot ❤️.
 
 
 Here you can get images of any dog breed and any no. of images you want,just read the instruction carefully!!`)
-  ctx.reply(
+ await  ctx.reply(
     `Enter the name of the dog breed + no. of images u want after the /get command (Example - /get hound 3)`
   );
 });
@@ -19,7 +19,7 @@ bot.help((ctx)=>{
     `Enter the name of the dog breed + no. of images u want after the /get command (Example - /get hound 3)`
   );
 })
-  bot.command('get',(ctx)=>{
+  bot.command('get',async (ctx)=>{
   let name = '';
   let arr = ctx.message.text.split(' ');
   console.log(ctx.from);
@@ -27,7 +27,7 @@ bot.help((ctx)=>{
     ctx.reply(
       `Enter the name of the dog breed + no. of images u want after the /get command (Example - /get hound 3)`
     );
-  } else {
+  } else { 
     let n = arr[2];
     console.log(n);
     name = arr[1];
@@ -41,7 +41,7 @@ bot.help((ctx)=>{
       url = `https://dog.ceo/api/breed/${name}/${subBreed}/images/random/${n}`;
     }
     // ctx.reply('You pressed the start command');
-    fetch(url)
+    await fetch(url)
       .then((response) => {
         if (!response.ok) {
           throw new Error('Check your spelling or command.');
